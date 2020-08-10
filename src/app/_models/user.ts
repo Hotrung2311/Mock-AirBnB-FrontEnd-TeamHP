f3ca5e2b3c01fe3459973b0443146151c961aebd
bangmac <anhbexd@gmail.com>
2020-08-10 23:09:36 +0700

    update

---
+++ src/app/_models/user.ts
@@ -0,0 +1,11 @@
+﻿export class User {
+    id: string;
+
+    email: string;
+    phone: string;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    token: string;
+}
