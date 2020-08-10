278522aa04be6d207a8997ed08452747168117f9
bangmac <anhbexd@gmail.com>
2020-08-10 10:51:26 +0700

    create login form done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    SignUpComponent,
   ],
   imports: [
     BrowserModule,
