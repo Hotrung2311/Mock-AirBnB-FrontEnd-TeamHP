73ff94ab8c111a4827df991d268549d0aed4cc7b
bangmac <anhbexd@gmail.com>
2020-08-12 21:32:32 +0700

    ..

--- src/app/host/product-status/product-status.module.ts
+++ src/app/host/product-status/product-status.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ProductsNotRentedComponent } from './products-not-rented/products-not-rented.component';
 import { ProductsRentedComponent } from './products-rented/products-rented.component';
 import { ProductsRentingComponent } from './products-renting/products-renting.component';
+import { LayoutComponent } from './layout/layout.component';
 
 
 @NgModule({
-  declarations: [ProductsNotRentedComponent, ProductsRentedComponent, ProductsRentingComponent],
+  declarations: [ProductsNotRentedComponent, ProductsRentedComponent, ProductsRentingComponent, LayoutComponent],
   imports: [
     CommonModule
   ]
