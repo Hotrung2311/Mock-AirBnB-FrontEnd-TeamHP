15e9e95d5ceda24556a06728784aa923f8f76b6e
bangmac <anhbexd@gmail.com>
2020-08-12 16:23:00 +0700

    add

--- src/app/accounts/accounts.module.ts
+++ src/app/accounts/accounts.module.ts
@@ -6,11 +6,13 @@ import {RouterModule, Routes} from "@angular/router";
 import {HomepageComponent} from "../home/homepage/homepage.component";
 import {AccountListComponent} from "../admin-page/account-manager/account-list/account-list.component";
 import {ProductManagerComponent} from "./product-manager/product-manager.component";
-import {LayoutComponent} from "@app/accounts/layout/layout.component";
-import {AccountLoginComponent} from "@app/accounts/account-login/account-login.component";
-import {AccountRegisterComponent} from "@app/accounts/account-register/account-register.component";
+// import {LayoutComponent} from "@app/accounts/layout/layout.component";
+// import {AccountLoginComponent} from "@app/accounts/account-login/account-login.component";
+// import {AccountRegisterComponent} from "@app/accounts/account-register/account-register.component";
 import {ReactiveFormsModule} from "@angular/forms";
-import {AccountRoutingModule} from "@app/accounts/account-routing.module";
+// import {AccountRoutingModule} from "@app/accounts/account-routing.module";
+import {AccountRegisterComponent} from "./account-register/account-register.component";
+import {AccountLoginComponent} from "./account-login/account-login.component";
 
 const routes: Routes = [
   {path: 'products', component: ProductManagerComponent},
