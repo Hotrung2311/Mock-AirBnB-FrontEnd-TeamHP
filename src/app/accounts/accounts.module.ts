258365e710b4924072b519f7a46f14a102c8e3ac
bangmac <anhbexd@gmail.com>
2020-08-12 15:56:19 +0700

    ,,,

--- src/app/accounts/accounts.module.ts
+++ src/app/accounts/accounts.module.ts
@@ -6,15 +6,27 @@ import {RouterModule, Routes} from "@angular/router";
 import {HomepageComponent} from "../home/homepage/homepage.component";
 import {AccountListComponent} from "../admin-page/account-manager/account-list/account-list.component";
 import {ProductManagerComponent} from "./product-manager/product-manager.component";
+import {LayoutComponent} from "@app/accounts/layout/layout.component";
+import {AccountLoginComponent} from "@app/accounts/account-login/account-login.component";
+import {AccountRegisterComponent} from "@app/accounts/account-register/account-register.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import {AccountRoutingModule} from "@app/accounts/account-routing.module";
 
 const routes: Routes = [
   {path: 'products', component: ProductManagerComponent},
 ]
 
 @NgModule({
-  declarations: [ImageManagerComponent, OrdersManagerComponent],
+  declarations: [
+    ImageManagerComponent,
+    OrdersManagerComponent,
+    LayoutComponent,
+    AccountLoginComponent,
+    AccountRegisterComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    AccountRoutingModule,
     RouterModule.forChild(routes),
   ]
 })
