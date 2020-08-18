d0ca502cff63c85401acc2253b6ace3535dd339b
Hotrung2311 <trungho2311@gmail.com>
2020-08-18 09:56:37 +0700

    hoàn thành trang chủ

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AppComponent} from "@app/app.component";
 
 const routes: Routes = [
   {path:'home', loadChildren: './home-client/home-client.module#HomeClientModule'},
   {path:'user', loadChildren: './user/user.module#UserModule'},
+  // {path:'user', loadChildren: () => import('./user/user.module').then(value => value.UserModule)},
 ]
 
 @NgModule({
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,22 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookingHistoryComponent } from './booking-history/booking-history.component';
 import { SearchComponent } from './search/search.component';
-import { RoomDetailComponent } from './room-detail/room-detail.component';
 import {RouterModule, Routes} from "@angular/router";
-import {HomePageComponent} from "../home-client/home-page/home-page.component";
-import {ListProductsComponent} from "../home-client/list-products/list-products.component";
 import {DetailProductComponent} from "../home-client/detail-product/detail-product.component";
 import {SearchProductComponent} from "../home-client/search-product/search-product.component";
-import {HomeClientModule} from "@app/home-client/home-client.module";
+import { BookingFormComponent } from './booking-form/booking-form.component';
 
 const routes: Routes = [
   {path: 'history/:id', component: BookingHistoryComponent},
   {path: 'search', component: SearchProductComponent},
   {path: ':id', component: DetailProductComponent},
+  {path: 'book' , component: BookingFormComponent},
 ]
 
 @NgModule({
-  declarations: [BookingHistoryComponent, SearchComponent, RoomDetailComponent],
+  declarations: [BookingHistoryComponent, SearchComponent, BookingFormComponent],
+  exports: [
+    BookingFormComponent
+  ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
